64001af67f6e0f39c51647a5818bf5ca0788cb8a
CW-Logothetis <cwlogo@pm.me>
2021-07-27 22:37:34 +0300

    add four images

--- index.js
+++ index.js
@@ -1,261 +1,261 @@
-// let dragonflies = [
-//     {
-//         "Family": "Calopterygidae", 
-//         "Scientific": "Calopteryx virgo", 
-//         "English": "Beautiful Demoiselle"}, 
-//     {
-//         "Family": "Calopterygidae", 
-//         "Scientific": "Calopterix virgo ssp. Meridionale"}, 
-//     {
-//         "Family": "Calopterygidae", 
-//         "Scientific": "Calopteryx splendens", 
-//         "English": "Banded demoiselle"}, 
-//     {
-//         "Family": "Calopterygidae", 
-//         "Scientific": "Calopterix splendens ssp. Balkanica"}, 
-//     {
-//         "Family": "Coenagrionidae", 
-//         "Scientific": "Erythromma najas", 
-//         "English": "Red-eyed damselfly"}, 
-//     {
-//         "Family": "Coenagrionidae", 
-//         "Scientific": "Erythromma viridulum", 
-//         "English": "Small red-eyed damselfly"}, 
-//     {
-//         "Family": "Coenagrionidae", 
-//         "Scientific": "Erythromma lindeni", 
-//         "English": "Goblet-marked damselfly"}, 
-//     {
-//         "Family": "Coenagrionidae", 
-//         "Scientific": "Enallagma cyathigerum", 
-//         "English": "Common blue damselfly"}, 
-//     {
-//         "Family": "Coenagrionidae", 
-//         "Scientific": "Ischnura elegans", 
-//         "English": "Blue-tailed damselfy"}, 
-//     {
-//         "Family": "Coenagrionidae", 
-//         "Scientific": "Ischnura pumilio", 
-//         "English": "Scarce Blue-tailed Damselfly "}, 
-//     {
-//         "Family": "Coenagrionidae", 
-//         "Scientific": "Ceriagrion tenellum", 
-//         "English": "Small red damselfly"}, 
-//     {
-//         "Family": "Coenagrionidae", 
-//         "Scientific": "Coenagrion ornatum", 
-//         "English": "Ornate bluet"}, 
-//     {
-//         "Family": "Coenagrionidae", 
-//         "Scientific": "Coenagrion puella", 
-//         "English": "Azure damselfly"}, 
-//     {
-//         "Family": "Coenagrionidae", 
-//         "Scientific": "Coenagrion pulchellum", 
-//         "English": "Variable damselfly"}, 
-//     {
-//         "Family": "Coenagrionidae", 
-//         "Scientific": "Coenagrion scitulum", 
-//         "English": "Dainty bluet"}, 
-//     {
-//         "Family": "Coenagrionidae", 
-//         "Scientific": "Pyrrhosoma elisabethae NW Gr CR", 
-//         "English": "Greek red damsel"}, 
-//     {
-//         "Family": "Coenagrionidae", 
-//         "Scientific": "Pyrrhosoma nymphula", 
-//         "English": "Large red damsel"}, 
-//     {
-//         "Family": "Lestidae", 
-//         "Scientific": "Chalcolestes (Lestes) viridis", 
-//         "English": "Emerald (W. Willow speadwing)"}, 
-//     {
-//         "Family": "Lestidae", 
-//         "Scientific": "Chalcolestes (Lestes) parvidens", 
-//         "English": "E. willow spreadwing"}, 
-//     {
-//         "Family": "Lestidae", 
-//         "Scientific": "Lestes barbarus", 
-//         "English": "S. emerald / Shy emerald"}, 
-//     {
-//         "Family": "Lestidae", 
-//         "Scientific": "Lestes macrostigma COAST", 
-//         "English": "Dark spreadwing"}, 
-//     {
-//         "Family": "Lestidae", 
-//         "Scientific": "Lestes virins", 
-//         "English": "Small (emerald) spreadwing"}, 
-//     {
-//         "Family": "Lestidae", 
-//         "Scientific": "Sympecma fusca", 
-//         "English": "Winter damsalfly"}, 
-//     {
-//         "Family": "Platycnmeididae", 
-//         "Scientific": "Platycnemis pennipes", 
-//         "English": "White-legged/ blue featherleg"}, 
-//     {
-//         "Family": "Aeshnidae", 
-//         "Scientific": "Aeshna affinis", 
-//         "English": "S. migrant / blue-eyed hawker"}, 
-//     {
-//         "Family": "Aeshnidae", 
-//         "Scientific": "Aeshna isosceles", 
-//         "English": "Green-eyed (Norfolk) hawker"}, 
-//     {
-//         "Family": "Aeshnidae", 
-//         "Scientific": "Aeshna mixta", 
-//         "English": "Migrant Hawker"}, 
-//     {
-//         "Family": "Aeshnidae", 
-//         "Scientific": "Anax ephippiger*", 
-//         "English": "Vagrant emperor"}, 
-//     {
-//         "Family": "Aeshnidae", 
-//         "Scientific": "Anax immaculifrons EAST", 
-//         "English": "Magnificent emperor EAST"}, 
-//     {
-//         "Family": "Aeshnidae", 
-//         "Scientific": "Anax imperator", 
-//         "English": "(Blue) Emperor dragonfly"}, 
-//     {
-//         "Family": "Aeshnidae", 
-//         "Scientific": "Anax parthenope", 
-//         "English": "Lesser emperor dragonfly"}, 
-//     {
-//         "Family": "Aeshnidae", 
-//         "Scientific": "Brachytron pratense", 
-//         "English": "Hairy hawker"}, 
-//     {
-//         "Family": "Aeshnidae", 
-//         "Scientific": "Caliaeshna microstigma", 
-//         "English": "Shady hawker"}, 
-//     {
-//         "Family": "Cordulegastridae", 
-//         "Scientific": "Cordulegaster bidentata NT", 
-//         "English": "Sombre goldenring"}, 
-//     {
-//         "Family": "Cordulegastridae", 
-//         "Scientific": "Cordulegaster picta EAST LC", 
-//         "English": "Turkish goldenring"}, 
-//     {
-//         "Family": "Cordulegastridae", 
-//         "Scientific": "Cordulegaster hel. ssp. Helladica EN", 
-//         "English": "Greek goldenring"}, 
-//     {
-//         "Family": "Cordulegastridae", 
-//         "Scientific": "Cordulegaster heros", 
-//         "English": "Balkan goldenring"}, 
-//     {
-//         "Family": "Cordulegastridae", 
-//         "Scientific": "Cordulegaster insignis", 
-//         "English": "Blue-eyed goldenring"}, 
-//     {
-//         "Family": "Corduliidae", 
-//         "Scientific": "Cordulia aenea", 
-//         "English": "Downy emerald"}, 
-//     {
-//         "Family": "Corduliidae", 
-//         "Scientific": "Somatochlora borisi EAST VU", 
-//         "English": "Bulgarian emperor"}, 
-//     {
-//         "Family": "Corduliidae", 
-//         "Scientific": "Somatochlora flavomaculata", 
-//         "English": "Yellow-spotted emperor"}, 
-//     {
-//         "Family": "Corduliidae", 
-//         "Scientific": "Somatochlora meidionalis", 
-//         "English": "Balkan emerald"}, 
-//     {
-//         "Family": "Gomphidae", 
-//         "Scientific": "Gomphus flavipes", 
-//         "English": "River clubtail"}, 
-//     {
-//         "Family": "Gomphidae", 
-//         "Scientific": "Gomphus schneiderii", 
-//         "English": "Turkish clubtail"}, 
-//     {
-//         "Family": "Gomphidae", 
-//         "Scientific": "Gomphus vulgatissimus", 
-//         "English": "Clubtail"}, 
-//     {
-//         "Family": "Gomphidae", 
-//         "Scientific": "Lindenia tetraphylla", 
-//         "English": "Bladetail"}, 
-//     {
-//         "Family": "Gomphidae", 
-//         "Scientific": "Onychogomphus forcipatus", 
-//         "English": "Small pincertail"}, 
-//     {
-//         "Family": "Libellulidae", 
-//         "Scientific": "Crocothemis erythraea", 
-//         "English": "Broad scarlet"}, 
-//     {
-//         "Family": "Libellulidae", 
-//         "Scientific": "Libellula depressa", 
-//         "English": "Broad-bodied chaser"}, 
-//     {
-//         "Family": "Libellulidae", 
-//         "Scientific": "Libellula fulva", 
-//         "English": "Blue chaser"}, 
-//     {
-//         "Family": "Libellulidae", 
-//         "Scientific": "Libellula quadrimaculata", 
-//         "English": "Four-spotted chaser"}, 
-//     {
-//         "Family": "Libellulidae", 
-//         "Scientific": "Orthetrum albistylum", 
-//         "English": "White-tailed skimmer"}, 
-//     {
-//         "Family": "Libellulidae", 
-//         "Scientific": "Orthetrum brunneum", 
-//         "English": "Southern skimmer"}, 
-//     {
-//         "Family": "Libellulidae", 
-//         "Scientific": "Orthetrum cancellatum", 
-//         "English": "Black-tailed skimmer"}, 
-//     {
-//         "Family": "Libellulidae", 
-//         "Scientific": "Orthetrum coerulescens", 
-//         "English": "Keeled skimmer"}, 
-//     {
-//         "Family": "Libellulidae", 
-//         "Scientific": "Orthetrum ramburi?"}, 
-//     {
-//         "Family": "Libellulidae", 
-//         "Scientific": "Selysiothemis nigra", 
-//         "English": "Black pennant"}, 
-//     {
-//         "Family": "Libellulidae", 
-//         "Scientific": "Sympetrum depressiusculum VU", 
-//         "English": "Spotted darter"}, 
-//     {
-//         "Family": "Libellulidae", 
-//         "Scientific": "Sympetrum flaveolum", 
-//         "English": "Yellow-winged darter"}, 
-//     {
-//         "Family": "Libellulidae", 
-//         "Scientific": "Sympetrum fonscolombei", 
-//         "English": "Red-veined darter"}, 
-//     {
-//         "Family": "Libellulidae", 
-//         "Scientific": "Sympetrum meridionale", 
-//         "English": "Southern darter"}, 
-//     {
-//         "Family": "Libellulidae", 
-//         "Scientific": "Sympetrum pedemontanum"}, 
-//     {
-//         "Family": "Libellulidae", 
-//         "Scientific": "Sympetrum sanguineum", 
-//         "English": "Ruddy darter"}, 
-//     {
-//         "Family": "Libellulidae", 
-//         "Scientific": "Sympetrum striolatum", 
-//         "English": "Common darter"}, 
-//     {
-//         "Family": "Libellulidae", 
-//         "Scientific": "Sympetrum vulgatum", 
-//         "English": "Vagrant darter"}
-// ]
+let dragonflies = [
+    {
+        "Family": "Calopterygidae", 
+        "Scientific": "Calopteryx virgo", 
+        "English": "Beautiful Demoiselle"}, 
+    {
+        "Family": "Calopterygidae", 
+        "Scientific": "Calopterix virgo ssp. Meridionale"}, 
+    {
+        "Family": "Calopterygidae", 
+        "Scientific": "Calopteryx splendens", 
+        "English": "Banded demoiselle"}, 
+    {
+        "Family": "Calopterygidae", 
+        "Scientific": "Calopterix splendens ssp. Balkanica"}, 
+    {
+        "Family": "Coenagrionidae", 
+        "Scientific": "Erythromma najas", 
+        "English": "Red-eyed damselfly"}, 
+    {
+        "Family": "Coenagrionidae", 
+        "Scientific": "Erythromma viridulum", 
+        "English": "Small red-eyed damselfly"}, 
+    {
+        "Family": "Coenagrionidae", 
+        "Scientific": "Erythromma lindeni", 
+        "English": "Goblet-marked damselfly"}, 
+    {
+        "Family": "Coenagrionidae", 
+        "Scientific": "Enallagma cyathigerum", 
+        "English": "Common blue damselfly"}, 
+    {
+        "Family": "Coenagrionidae", 
+        "Scientific": "Ischnura elegans", 
+        "English": "Blue-tailed damselfy"}, 
+    {
+        "Family": "Coenagrionidae", 
+        "Scientific": "Ischnura pumilio", 
+        "English": "Scarce Blue-tailed Damselfly "}, 
+    {
+        "Family": "Coenagrionidae", 
+        "Scientific": "Ceriagrion tenellum", 
+        "English": "Small red damselfly"}, 
+    {
+        "Family": "Coenagrionidae", 
+        "Scientific": "Coenagrion ornatum", 
+        "English": "Ornate bluet"}, 
+    {
+        "Family": "Coenagrionidae", 
+        "Scientific": "Coenagrion puella", 
+        "English": "Azure damselfly"}, 
+    {
+        "Family": "Coenagrionidae", 
+        "Scientific": "Coenagrion pulchellum", 
+        "English": "Variable damselfly"}, 
+    {
+        "Family": "Coenagrionidae", 
+        "Scientific": "Coenagrion scitulum", 
+        "English": "Dainty bluet"}, 
+    {
+        "Family": "Coenagrionidae", 
+        "Scientific": "Pyrrhosoma elisabethae NW Gr CR", 
+        "English": "Greek red damsel"}, 
+    {
+        "Family": "Coenagrionidae", 
+        "Scientific": "Pyrrhosoma nymphula", 
+        "English": "Large red damsel"}, 
+    {
+        "Family": "Lestidae", 
+        "Scientific": "Chalcolestes (Lestes) viridis", 
+        "English": "Emerald (W. Willow speadwing)"}, 
+    {
+        "Family": "Lestidae", 
+        "Scientific": "Chalcolestes (Lestes) parvidens", 
+        "English": "E. willow spreadwing"}, 
+    {
+        "Family": "Lestidae", 
+        "Scientific": "Lestes barbarus", 
+        "English": "S. emerald / Shy emerald"}, 
+    {
+        "Family": "Lestidae", 
+        "Scientific": "Lestes macrostigma COAST", 
+        "English": "Dark spreadwing"}, 
+    {
+        "Family": "Lestidae", 
+        "Scientific": "Lestes virins", 
+        "English": "Small (emerald) spreadwing"}, 
+    {
+        "Family": "Lestidae", 
+        "Scientific": "Sympecma fusca", 
+        "English": "Winter damsalfly"}, 
+    {
+        "Family": "Platycnmeididae", 
+        "Scientific": "Platycnemis pennipes", 
+        "English": "White-legged/ blue featherleg"}, 
+    {
+        "Family": "Aeshnidae", 
+        "Scientific": "Aeshna affinis", 
+        "English": "S. migrant / blue-eyed hawker"}, 
+    {
+        "Family": "Aeshnidae", 
+        "Scientific": "Aeshna isosceles", 
+        "English": "Green-eyed (Norfolk) hawker"}, 
+    {
+        "Family": "Aeshnidae", 
+        "Scientific": "Aeshna mixta", 
+        "English": "Migrant Hawker"}, 
+    {
+        "Family": "Aeshnidae", 
+        "Scientific": "Anax ephippiger*", 
+        "English": "Vagrant emperor"}, 
+    {
+        "Family": "Aeshnidae", 
+        "Scientific": "Anax immaculifrons EAST", 
+        "English": "Magnificent emperor EAST"}, 
+    {
+        "Family": "Aeshnidae", 
+        "Scientific": "Anax imperator", 
+        "English": "(Blue) Emperor dragonfly"}, 
+    {
+        "Family": "Aeshnidae", 
+        "Scientific": "Anax parthenope", 
+        "English": "Lesser emperor dragonfly"}, 
+    {
+        "Family": "Aeshnidae", 
+        "Scientific": "Brachytron pratense", 
+        "English": "Hairy hawker"}, 
+    {
+        "Family": "Aeshnidae", 
+        "Scientific": "Caliaeshna microstigma", 
+        "English": "Shady hawker"}, 
+    {
+        "Family": "Cordulegastridae", 
+        "Scientific": "Cordulegaster bidentata NT", 
+        "English": "Sombre goldenring"}, 
+    {
+        "Family": "Cordulegastridae", 
+        "Scientific": "Cordulegaster picta EAST LC", 
+        "English": "Turkish goldenring"}, 
+    {
+        "Family": "Cordulegastridae", 
+        "Scientific": "Cordulegaster hel. ssp. Helladica EN", 
+        "English": "Greek goldenring"}, 
+    {
+        "Family": "Cordulegastridae", 
+        "Scientific": "Cordulegaster heros", 
+        "English": "Balkan goldenring"}, 
+    {
+        "Family": "Cordulegastridae", 
+        "Scientific": "Cordulegaster insignis", 
+        "English": "Blue-eyed goldenring"}, 
+    {
+        "Family": "Corduliidae", 
+        "Scientific": "Cordulia aenea", 
+        "English": "Downy emerald"}, 
+    {
+        "Family": "Corduliidae", 
+        "Scientific": "Somatochlora borisi EAST VU", 
+        "English": "Bulgarian emperor"}, 
+    {
+        "Family": "Corduliidae", 
+        "Scientific": "Somatochlora flavomaculata", 
+        "English": "Yellow-spotted emperor"}, 
+    {
+        "Family": "Corduliidae", 
+        "Scientific": "Somatochlora meidionalis", 
+        "English": "Balkan emerald"}, 
+    {
+        "Family": "Gomphidae", 
+        "Scientific": "Gomphus flavipes", 
+        "English": "River clubtail"}, 
+    {
+        "Family": "Gomphidae", 
+        "Scientific": "Gomphus schneiderii", 
+        "English": "Turkish clubtail"}, 
+    {
+        "Family": "Gomphidae", 
+        "Scientific": "Gomphus vulgatissimus", 
+        "English": "Clubtail"}, 
+    {
+        "Family": "Gomphidae", 
+        "Scientific": "Lindenia tetraphylla", 
+        "English": "Bladetail"}, 
+    {
+        "Family": "Gomphidae", 
+        "Scientific": "Onychogomphus forcipatus", 
+        "English": "Small pincertail"}, 
+    {
+        "Family": "Libellulidae", 
+        "Scientific": "Crocothemis erythraea", 
+        "English": "Broad scarlet"}, 
+    {
+        "Family": "Libellulidae", 
+        "Scientific": "Libellula depressa", 
+        "English": "Broad-bodied chaser"}, 
+    {
+        "Family": "Libellulidae", 
+        "Scientific": "Libellula fulva", 
+        "English": "Blue chaser"}, 
+    {
+        "Family": "Libellulidae", 
+        "Scientific": "Libellula quadrimaculata", 
+        "English": "Four-spotted chaser"}, 
+    {
+        "Family": "Libellulidae", 
+        "Scientific": "Orthetrum albistylum", 
+        "English": "White-tailed skimmer"}, 
+    {
+        "Family": "Libellulidae", 
+        "Scientific": "Orthetrum brunneum", 
+        "English": "Southern skimmer"}, 
+    {
+        "Family": "Libellulidae", 
+        "Scientific": "Orthetrum cancellatum", 
+        "English": "Black-tailed skimmer"}, 
+    {
+        "Family": "Libellulidae", 
+        "Scientific": "Orthetrum coerulescens", 
+        "English": "Keeled skimmer"}, 
+    {
+        "Family": "Libellulidae", 
+        "Scientific": "Orthetrum ramburi?"}, 
+    {
+        "Family": "Libellulidae", 
+        "Scientific": "Selysiothemis nigra", 
+        "English": "Black pennant"}, 
+    {
+        "Family": "Libellulidae", 
+        "Scientific": "Sympetrum depressiusculum VU", 
+        "English": "Spotted darter"}, 
+    {
+        "Family": "Libellulidae", 
+        "Scientific": "Sympetrum flaveolum", 
+        "English": "Yellow-winged darter"}, 
+    {
+        "Family": "Libellulidae", 
+        "Scientific": "Sympetrum fonscolombei", 
+        "English": "Red-veined darter"}, 
+    {
+        "Family": "Libellulidae", 
+        "Scientific": "Sympetrum meridionale", 
+        "English": "Southern darter"}, 
+    {
+        "Family": "Libellulidae", 
+        "Scientific": "Sympetrum pedemontanum"}, 
+    {
+        "Family": "Libellulidae", 
+        "Scientific": "Sympetrum sanguineum", 
+        "English": "Ruddy darter"}, 
+    {
+        "Family": "Libellulidae", 
+        "Scientific": "Sympetrum striolatum", 
+        "English": "Common darter"}, 
+    {
+        "Family": "Libellulidae", 
+        "Scientific": "Sympetrum vulgatum", 
+        "English": "Vagrant darter"}
+]
 
 document.getElementById("search-input").addEventListener("keyup", function(event) {
 let searchQuery = event.target.value.toLowerCase()
